bd7e997136c5d741d849b298ca95ed412de8f648
Brandon Weaver <a7xncob@gmail.com>
2019-11-03 19:58:38 -0500

    Importing project

---
+++ TicTacToeConsole/Game.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <iostream>
+
+class Game
+{
+public:
+	Game();
+	// Constructor->Game: Initializes each element of the dynamic array '_board' with a single whitespace character.
+
+	void Play();
+	// Play: A single game loop.
+
+private:
+	const int WIN_COMBINATIONS[8][3] = { {0,1,2}, {3,4,5}, {6,7,8},
+										 {0,3,6}, {1,4,7}, {2,5,8},
+										 {0,4,8}, {2,4,6} };
+	// WIN_COMBINATIONS: Combinations of '_board' indices which define a winning set.
+
+	char* _board;
+	// _board: An array of characters which the playable area of the board is comprised of.
+
+	//  + | + | + 
+	// -----------
+	//  + | + | + 
+	// -----------
+	//  + | + | + 
+
+	int InputToIndex();
+	// InputToIndex: Prompts the user for input and converts the input, 1-9, to a '_board' index, 0-8.
+
+	bool IsValidMove(int);
+	// IsValidMove(_board->index): Returns true only when the '_board->index' is available.
+
+	int Turn();
+	// Turn: Returns the number of turns taken.
+
+	char Token();
+	// Token: Returns the current token, 'O' or 'X'.
+
+	void Move(int, char);
+	// Move(_board->index, token): Places the 'token' in the 'board->index'.
+
+	bool IsGameOver();
+	// IsGameOver: Returns true if the game is over.
+
+	friend std::ostream& operator << (std::ostream&, Game&);
+	// Operator-><<: Displays the '_board' in an aesthetic manner.
+};
---
+++ TicTacToeConsole/GameImp.cpp
@@ -0,0 +1,79 @@
+#include "Game.h"
+
+Game::Game()
+{
+	this->_board = new char[9] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+}
+
+void Game::Play()
+{
+	do
+	{
+		std::cout << *this << "\nEnter 1-9: ";
+		int index = this->InputToIndex();
+		if (this->IsValidMove(index))
+			this->Move(index, this->Token());
+	} while (!this->IsGameOver());
+}
+
+int Game::InputToIndex()
+{
+	int input;
+	std::cin >> input;
+	return input - 1;
+}
+
+bool Game::IsValidMove(int index)
+{
+	if (index < 0 || index > 8)
+		return false;
+	if (this->_board[index] == 'O' || this->_board[index] == 'X')
+		return false;
+	return true;
+}
+
+int Game::Turn()
+{
+	int count = 0;
+	for (int i = 0; i < 9; i++)
+		if (this->_board[i] == 'O' || this->_board[i] == 'X')
+			count++;
+	return count;
+}
+
+char Game::Token()
+{
+	return this->Turn() % 2 != 0 ? 'O' : 'X';
+}
+
+void Game::Move(int index, char token)
+{
+	this->_board[index] = token;
+}
+
+bool Game::IsGameOver()
+{
+	if (this->Turn() == 9)
+		return true;
+	for (int i = 0; i < sizeof(this->WIN_COMBINATIONS) / sizeof(*this->WIN_COMBINATIONS); i++)
+	{
+		int index1 = this->WIN_COMBINATIONS[i][0];
+		int index2 = this->WIN_COMBINATIONS[i][1];
+		int index3 = this->WIN_COMBINATIONS[i][2];
+		if (this->_board[index1] == 'O' && this->_board[index2] == 'O' && this->_board[index3] == 'O')
+			return true;
+		if (this->_board[index1] == 'X' && this->_board[index2] == 'X' && this->_board[index3] == 'X')
+			return true;
+	}
+	return false;
+}
+
+std::ostream& operator << (std::ostream& out, Game& game)
+{
+	out << ' ' << game._board[0] << ' ' << '|' << ' ' << game._board[1] << ' ' << '|' << ' ' << game._board[2] << ' ' << '\n'
+		<< "-----------\n"
+		<< ' ' << game._board[3] << ' ' << '|' << ' ' << game._board[4] << ' ' << '|' << ' ' << game._board[5] << ' ' << '\n'
+		<< "-----------\n"
+		<< ' ' << game._board[6] << ' ' << '|' << ' ' << game._board[7] << ' ' << '|' << ' ' << game._board[8] << ' ' << '\n';
+	return out;
+}
